a6c0f6aab7cf805b61c3b69d3a2f372d662a7bce
Hendrik <dev.nipe.systems@gmail.com>
2020-05-12 18:58:02 +0200

    Add hashes to widgets, other improvements

--- api/ApiServer.js
+++ api/ApiServer.js
@@ -1,9 +1,9 @@
 const path = require('path');
 const url = require('url');
+const crypto = require('crypto');
 const EventEmitter = require('events');
 const express = require('express');
 const SseStream = require('ssestream');
-const hashFromPageUuidAndUrl = require('./hashFromPageUuidAndUrl');
 
 class ApiServer extends EventEmitter {
   constructor() {
@@ -21,7 +21,7 @@ class ApiServer extends EventEmitter {
       stream.pipe(response);
       stream.write({
         event: 'pages',
-        data: this.pages,
+        data: Object.keys(this.pages),
       });
       response.on('close', () => {
         this.pagesStreams = this.pagesStreams.filter(pagesStream => pagesStream !== stream);
@@ -32,37 +32,43 @@ class ApiServer extends EventEmitter {
       // TODO: test that client stream is added to pagesStream (and removed if disconnected)
       // TODO: test 404 and 400 (negatives and positives)
       // TODO: test that server emits 'widgets' event on connect
-      const requestUrl = new url.URL(path.join(path.sep, path.relative('/page', request.url)), 'http://localhost/');
-      const pageUrl = requestUrl.pathname;
+      // TODO: test that hashes are unique
+      const requestUrl = new url.URL(request.url, 'http://localhost/');
+      const pageUrl = path.join(path.sep, path.relative('/page', requestUrl.pathname));
       if (!this.pages[pageUrl]) {
-        console.log(`${request.url} -> 404`);
+        console.log(`${request.url} (${pageUrl}) -> 404`);
         response.sendStatus(404);
         return;
       }
       if (!requestUrl.searchParams.get('pageUuid')) {
-        console.log(`${request.url} -> 400`);
+        console.log(`${request.url} (${pageUrl}) -> 400`);
         response.sendStatus(400);
         return;
       }
       const stream = new SseStream(request);
       const pageUuid = requestUrl.searchParams.get('pageUuid');
-      const pageHash = hashFromPageUuidAndUrl(pageUuid, pageUrl);
+      const pageHash = crypto.createHash('sha256').update(`${pageUuid} ${pageUrl}`).digest('hex');
+      const pageStream = {
+        stream: stream,
+        uuid: pageUuid,
+        url: pageUrl,
+      };
       if (!this.pageStreams[pageHash])
         this.emit('instantiate', pageUuid, pageUrl);
       this.pageStreams = {
         ...this.pageStreams,
-        [pageHash]: [...(this.pageStreams[pageHash] || []), stream],
+        [pageHash]: [...(this.pageStreams[pageHash] || []), pageStream],
       };
       stream.pipe(response);
       stream.write({
         event: 'widgets',
-        data: this.pages[pageUrl].widgets,
+        data: this.convertWidgets(pageUuid, pageUrl),
       });
       response.on('close', () => {
         this.pageStreams = Object.keys(this.pageStreams).reduce((pageStreams, currentPageHash) => ({
           ...pageStreams,
           ...(currentPageHash === pageHash && this.pageStreams[currentPageHash].length === 1 ? {} : {
-            [currentPageHash]: this.pageStreams[currentPageHash].filter(pageStream => pageStream !== stream),
+            [currentPageHash]: this.pageStreams[currentPageHash].filter(currentPageStream => currentPageStream !== pageStream),
           }),
         }), {});
         stream.unpipe(response);
@@ -84,17 +90,54 @@ class ApiServer extends EventEmitter {
     for (const pagesStream of this.pagesStreams) {
       pagesStream.write({
         event: 'pages',
-        data: pages,
+        data: Object.keys(pages),
       });
     }
     for (const pageHash of Object.keys(this.pageStreams)) {
       for (const pageStream of this.pageStreams[pageHash]) {
-        pageStream.write({
+        pageStream.stream.write({
           event: 'pages',
-          data: pages,
+          data: Object.keys(pages),
+        });
+        pageStream.stream.write({
+          event: 'widgets',
+          data: this.convertWidgets(pageStream.uuid, pageStream.url),
         });
       }
     }
+  }
+  convertWidgets(pageUuid, pageUrl) {
+    return this.pages[pageUrl].widgets.map((widget, index) => {
+      switch (widget.type) {
+        case 'text':
+          return {
+            ...widget,
+            hash: crypto.createHash('sha256').update(`${pageUuid} ${pageUrl} ${widget.file} ${index}`).digest('hex'),
+          };
+        case 'image':
+          return {
+            ...widget,
+            hash: crypto.createHash('sha256').update(`${pageUuid} ${pageUrl} ${widget.file} ${index}`).digest('hex'),
+          };
+        case 'button':
+          return {
+            ...widget,
+            hash: crypto.createHash('sha256').update(`${pageUuid} ${pageUrl} ${widget.label} ${widget.command} ${index}`).digest('hex'),
+          };
+        case 'editor':
+          return {
+            ...widget,
+            hash: crypto.createHash('sha256').update(`${pageUuid} ${pageUrl} ${widget.file} ${index}`).digest('hex'),
+          };
+        case 'terminal':
+          return {
+            ...widget,
+            hash: crypto.createHash('sha256').update(`${pageUuid} ${pageUrl} ${widget.workingDirectory} ${index}`).digest('hex'),
+          };
+        default:
+          return widget;
+      }
+    });
   }
 }
 
--- api/test.js
+++ api/test.js
@@ -1,20 +1,30 @@
 const ApiServer = require('./ApiServer');
 const apiServer = new ApiServer();
 apiServer.setPages({
-  '/': {},
+  '/': {
+    widgets: [
+      { type: 'markdown', content: '# Yo this works!' },
+      { type: 'text', file: '/test.txt' },
+      { type: 'image', file: '/test.png' },
+      { type: 'button', label: 'A button', command: 'date' },
+      { type: 'editor', file: '/test.txt' },
+      { type: 'terminal', workingDirectory: '/test' },
+    ],
+  },
   '/run': {
     widgets: [
       { type: 'markdown', content: '# Yo this works!' },
       { type: 'text', file: '/test.txt' },
+      { type: 'image', file: '/test.png' },
+      { type: 'button', label: 'A button', command: 'date' },
+      { type: 'editor', file: '/test.txt' },
+      { type: 'terminal', workingDirectory: '/test' },
     ],
   },
 });
-// setTimeout(() => {
-//   apiServer.setPages({
-//     '/': {},
-//     '/run': {},
-//   });
-// }, 10000);
+setTimeout(() => {
+  apiServer.setPages(apiServer.pages);
+}, 10000);
 apiServer.on('instantiate', (pageUuid, url) => {
   console.log('instantiate:', pageUuid, url);
 });
