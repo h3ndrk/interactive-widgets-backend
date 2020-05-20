dba576fa10071894bd36f1ad5aa206f0077f99a7
Hendrik <dev.nipe.systems@gmail.com>
2020-05-20 12:28:13 +0200

    Implement signal-reactive main loop

--- monitor-write/golang/main.go
+++ monitor-write/golang/main.go
@@ -1,18 +1,61 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
 	done := make(chan struct{})
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		fmt.Fprintln(os.Stderr, "Received signal:", <-signals)
+		close(done)
+	}()
 	pathToWatch := "/home/hendrik/Documents/containerized-playground/b/b/c/test.txt"
-	encoded, err := readFileToBase64(pathToWatch)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Print(encoded)
-	if err := waitForEvent(pathToWatch, done); err != nil {
-		log.Fatal(err)
+	lastEncoded := ""
+	// we need to first output an empty line because otherwise we don't output an empty line when the read results in an error
+	fmt.Println(lastEncoded)
+	for {
+		encoded, err := readFileToBase64(pathToWatch)
+		// encoded will be empty or containing the base64 string
+		if encoded != lastEncoded {
+			fmt.Println(encoded)
+			lastEncoded = encoded
+		}
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			timer := time.NewTimer(5 * time.Second)
+			select {
+			case <-timer.C:
+			case <-done:
+				if !timer.Stop() {
+					<-timer.C
+				}
+			}
+			if _, ok := <-done; !ok {
+				break
+			}
+			continue
+		}
+		if err := waitForEvent(pathToWatch, done); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			timer := time.NewTimer(5 * time.Second)
+			select {
+			case <-timer.C:
+			case <-done:
+				if !timer.Stop() {
+					<-timer.C
+				}
+			}
+			if _, ok := <-done; !ok {
+				break
+			}
+			continue
+		}
 	}
 }
