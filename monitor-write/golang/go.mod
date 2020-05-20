988e4f0882280b2703b01571ef57434db3aa9b95
Hendrik <dev.nipe.systems@gmail.com>
2020-05-20 10:57:03 +0200

    Add golang monitor application

---
+++ monitor-write/golang/go.mod
@@ -0,0 +1,5 @@
+module github.com/containerized-playground/monitor-write/golang
+
+go 1.14
+
+require github.com/fsnotify/fsnotify v1.4.9 // indirect
---
+++ monitor-write/golang/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"log"
+	"path/filepath"
+
+	"github.com/fsnotify/fsnotify"
+)
+
+func waitForEvent(pathToWatch string, done <-chan struct{}) error {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	if err := watcher.Add(pathToWatch); err != nil {
+		return err
+	}
+	var parentPaths []string
+	currentPath := pathToWatch
+	for filepath.Dir(currentPath) != currentPath {
+		currentPath = filepath.Dir(currentPath)
+		parentPaths = append(parentPaths, currentPath)
+	}
+	for _, parentPath := range parentPaths {
+		if err := watcher.Add(parentPath); err != nil {
+			return err
+		}
+	}
+
+	for {
+		select {
+		case _, ok := <-done:
+			if !ok {
+				return nil
+			}
+		case event, ok := <-watcher.Events:
+			if !ok {
+				return nil
+			}
+			if event.Name == pathToWatch && event.Op&(fsnotify.Create|fsnotify.Write|fsnotify.Remove|fsnotify.Rename) != 0 {
+				return nil
+			}
+			for _, parentPath := range parentPaths {
+				if event.Name == parentPath && event.Op&(fsnotify.Create|fsnotify.Write|fsnotify.Remove|fsnotify.Rename) != 0 {
+					return nil
+				}
+			}
+		case err, ok := <-watcher.Errors:
+			if !ok {
+				return nil
+			}
+			return err
+		}
+	}
+}
+
+func main() {
+	done := make(chan struct{})
+	if err := waitForEvent("/home/hendrik/Documents/containerized-playground/b/b/c/test.txt", done); err != nil {
+		log.Fatal(err)
+	}
+}
