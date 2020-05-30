80485b93424dd4d1ed4180ef75dc7bd1e40cc7da
Hendrik <dev.nipe.systems@gmail.com>
2020-05-30 10:38:10 +0200

    Add client interface

---
+++ internal/server/client.go
@@ -0,0 +1,16 @@
+package server
+
+import "github.com/h3ndrk/containerized-playground/internal/id"
+
+// Client represents a client connecting to a page instance.
+type Client interface {
+	// Read returns messages sent by the client. If there is no data available
+	// at the moment, Read blocks until data is available again. If the client
+	// disconnects, Read unblocks and returns io.EOF (also for future calls).
+	Read() (id.WidgetID, []byte, error)
+
+	// Write sends given data of a given widget to the client. If the client
+	// cannot receive any data currently, Write blocks until the data has been
+	// sent to the client.
+	Write(id.WidgetID, []byte) error
+}
