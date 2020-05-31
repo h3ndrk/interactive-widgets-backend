169c2624e7573dde7522407ec571c8db91e1807e
Hendrik <dev.nipe.systems@gmail.com>
2020-05-31 22:07:29 +0200

    Reimplement channels in multiplexer again

--- internal/multiplexer/multiplexer.go
+++ internal/multiplexer/multiplexer.go
@@ -8,9 +8,14 @@ import (
 	"github.com/h3ndrk/containerized-playground/internal/executor"
 	"github.com/h3ndrk/containerized-playground/internal/id"
 	"github.com/h3ndrk/containerized-playground/internal/parser"
-	"github.com/hashicorp/go-multierror"
 	"github.com/pkg/errors"
 )
+
+type attachRequest struct {
+	pageID     id.PageID
+	client     Client
+	errChannel chan error
+}
 
 // Multiplexer attaches/detaches multiple clients to one page instance of an
 // executor and forwards messages back and forth between the clients and the
@@ -19,28 +24,213 @@ type Multiplexer struct {
 	executor executor.Executor
 	pages    []parser.Page
 
-	startedPagesMutex sync.Mutex
-	startedPagesLocks pageLock
-	startedPages      map[id.PageID][]Client
+	attachRequestChannel chan attachRequest
 }
 
 type message struct {
-	WidgetID id.WidgetID
-	Data     []byte
+	widgetID id.WidgetID
+	data     []byte
+}
+
+type detachRequest struct {
+	pageID id.PageID
+	client Client
 }
 
 // NewMultiplexer creates a new multiplexer based on an executor and the
 // available pages.
 func NewMultiplexer(pages []parser.Page, executor executor.Executor) (*Multiplexer, error) {
 	// TODO: add context to all New*() functions to be able to tear down the whole process
+
+	attachRequestChannel := make(chan attachRequest)
+	go func() {
+		startedPages := map[id.PageID][]Client{}
+		readFromClientsChannel := make(chan message)
+		readFromWidgetsChannel := make(chan message)
+		detachRequestChannel := make(chan detachRequest)
+		for {
+			select {
+			case request := <-attachRequestChannel:
+				if _, ok := startedPages[request.pageID]; !ok {
+					pageURL, roomID, err := id.PageURLAndRoomIDFromPageID(request.pageID)
+					if err != nil {
+						request.errChannel <- errors.Wrapf(err, "Failed to decode page ID \"%s\"", request.pageID)
+						close(request.errChannel)
+						break
+					}
+
+					page := parser.PageFromPageURL(pages, pageURL)
+					if page == nil {
+						request.errChannel <- errors.Errorf("No page with URL \"%s\"", pageURL)
+						close(request.errChannel)
+						break
+					}
+
+					if !page.IsInteractive {
+						request.errChannel <- errors.Errorf("Page \"%s\" is not interactive", pageURL)
+						close(request.errChannel)
+						break
+					}
+
+					widgetIDs := map[id.WidgetIndex]id.WidgetID{}
+					gotError := false
+					for widgetIndex, widget := range page.Widgets {
+						if !widget.IsInteractive() {
+							continue
+						}
+
+						widgetID, err := id.WidgetIDFromPageURLAndRoomIDAndWidgetIndex(pageURL, roomID, id.WidgetIndex(widgetIndex))
+						if err != nil {
+							request.errChannel <- errors.Wrapf(err, "Failed to encode widget ID for page \"%s\"", request.pageID)
+							close(request.errChannel)
+							gotError = true
+							break
+						}
+
+						widgetIDs[id.WidgetIndex(widgetIndex)] = widgetID
+					}
+					if gotError {
+						break
+					}
+
+					if err := executor.StartPage(request.pageID); err != nil {
+						request.errChannel <- errors.Wrapf(err, "Failed to start page \"%s\"", err)
+						close(request.errChannel)
+						break
+					}
+
+					for widgetIndex, widget := range page.Widgets {
+						if !widget.IsInteractive() {
+							continue
+						}
+
+						widgetID := widgetIDs[id.WidgetIndex(widgetIndex)]
+
+						go func(widget *parser.Widget, widgetID id.WidgetID) {
+							for {
+								data, err := executor.Read(widgetID)
+								if err != nil {
+									if err == io.EOF {
+										return
+									}
+
+									log.Print(err) // there is no error channel to the clients, just log it
+									return
+								}
+
+								readFromWidgetsChannel <- message{
+									widgetID: widgetID,
+									data:     data,
+								}
+							}
+						}(&page.Widgets[widgetIndex], widgetID)
+					}
+				}
+
+				startedPages[request.pageID] = append(startedPages[request.pageID], request.client)
+
+				go func() {
+					defer func() {
+						detachRequestChannel <- detachRequest{
+							pageID: request.pageID,
+							client: request.client,
+						}
+					}()
+
+					for {
+						widgetID, data, err := request.client.Read()
+						if err != nil {
+							if err == io.EOF {
+								return
+							}
+
+							log.Print(err) // there is no error channel to the clients, just log it
+							return
+						}
+
+						readFromClientsChannel <- message{
+							widgetID: widgetID,
+							data:     data,
+						}
+					}
+				}()
+
+				close(request.errChannel)
+			case message := <-readFromClientsChannel:
+				if err := executor.Write(message.widgetID, message.data); err != nil {
+					log.Print(err) // there is no error channel to the clients, just log it
+				}
+			case message := <-readFromWidgetsChannel:
+				sendMessageToAttachedClients(message, startedPages)
+			case request := <-detachRequestChannel:
+				n := 0
+				for _, c := range startedPages[request.pageID] {
+					if c != request.client {
+						startedPages[request.pageID][n] = c
+						n++
+					}
+				}
+				startedPages[request.pageID] = startedPages[request.pageID][:n]
+
+				if len(startedPages[request.pageID]) == 0 {
+					done := make(chan struct{})
+					go func() {
+						if err := executor.StopPage(request.pageID); err != nil {
+							log.Print(err) // there is no error channel to the clients, just log it
+							// continue to remove it
+						}
+						done <- struct{}{}
+					}()
+
+					// while waiting for StopPage, further process readFromWidgetsChannel (and drain the stopping page)
+				loop:
+					for {
+						select {
+						case message := <-readFromWidgetsChannel:
+							sendMessageToAttachedClients(message, startedPages)
+						case <-done:
+							break loop
+						}
+					}
+
+					delete(startedPages, request.pageID)
+				}
+			}
+		}
+	}()
+
 	return &Multiplexer{
-		executor: executor,
-		pages:    pages,
-		startedPagesLocks: pageLock{
-			pageLocks: map[id.PageID]*sync.Mutex{},
-		},
-		startedPages: map[id.PageID][]Client{},
+		executor:             executor,
+		pages:                pages,
+		attachRequestChannel: attachRequestChannel,
 	}, nil
+}
+
+func sendMessageToAttachedClients(message message, startedPages map[id.PageID][]Client) {
+	pageURL, roomID, _, err := id.PageURLAndRoomIDAndWidgetIndexFromWidgetID(message.widgetID)
+	if err != nil {
+		log.Print(err) // there is no error channel to the clients, just log it
+		return
+	}
+	pageID, err := id.PageIDFromPageURLAndRoomID(pageURL, roomID)
+	if err != nil {
+		log.Print(err) // there is no error channel to the clients, just log it
+		return
+	}
+
+	var writeWaiting sync.WaitGroup
+	writeWaiting.Add(len(startedPages[pageID]))
+	for clientIndex := range startedPages[pageID] {
+		go func(clientIndex int, writeWaiting *sync.WaitGroup) {
+			defer writeWaiting.Done()
+
+			if err := startedPages[pageID][clientIndex].Write(message.widgetID, message.data); err != nil {
+				log.Print(err) // there is no error channel to the clients, just log it
+			}
+		}(clientIndex, &writeWaiting)
+	}
+
+	writeWaiting.Wait()
 }
 
 // Attach attaches a given client to a given page ID. If the page instance
@@ -50,161 +240,16 @@ func NewMultiplexer(pages []parser.Page, executor executor.Executor) (*Multiplex
 // stopped. It is safe to call this function with same page ID arguments from
 // different goroutines.
 func (m *Multiplexer) Attach(pageID id.PageID, client Client) error {
-	pageURL, roomID, err := id.PageURLAndRoomIDFromPageID(pageID)
-	if err != nil {
-		return errors.Wrapf(err, "Failed to decode page ID \"%s\"", pageID)
+	errChannel := make(chan error)
+	m.attachRequestChannel <- attachRequest{
+		pageID:     pageID,
+		client:     client,
+		errChannel: errChannel,
 	}
 
-	page := parser.PageFromPageURL(m.pages, pageURL)
-	if page == nil {
-		return errors.Errorf("No page with URL \"%s\"", pageURL)
+	if err := <-errChannel; err != nil {
+		return errors.Wrapf(err, "Failed to attach client to page \"%s\"", pageID)
 	}
-
-	if !page.IsInteractive {
-		return errors.Errorf("Page \"%s\" is not interactive", pageURL)
-	}
-
-	// First, blockingly lock this page (this prevents concurrent instantiation
-	// or teardown of pages; once teardown is in progress, the lock is kept as
-	// long as teardown is in progress)
-	m.startedPagesLocks.lock(pageID)
-	defer m.startedPagesLocks.unlock(pageID)
-
-	// Second, lock the whole map of started pages
-	m.startedPagesMutex.Lock()
-	defer m.startedPagesMutex.Unlock()
-
-	// At this point: No teardown of this page and no other modification of the
-	// map of started pages is in progress. We can now modify the map of
-	// started pages and can safely add clients to the current page (will be
-	// added to an started page, not to a page that is currently in teardown).
-	attachedClients, ok := m.startedPages[pageID]
-	if !ok {
-		if err := m.executor.StartPage(pageID); err != nil {
-			return errors.Wrapf(err, "Failed to start page \"%s\"", err)
-		}
-
-		// stores the zero value (no attached clients) in the started pages
-		m.startedPages[pageID] = attachedClients
-
-		// connect all widgets of the page to mux channel
-		widgetIDs := map[id.WidgetIndex]id.WidgetID{}
-		for widgetIndex, widget := range page.Widgets {
-			if !widget.IsInteractive() {
-				continue
-			}
-
-			var cumulatedErrors error
-			widgetID, err := id.WidgetIDFromPageURLAndRoomIDAndWidgetIndex(pageURL, roomID, id.WidgetIndex(widgetIndex))
-			if err != nil {
-				cumulatedErrors = multierror.Append(cumulatedErrors, errors.Wrapf(err, "Failed to encode widget ID for page \"%s\"", pageID))
-
-				if err := m.executor.StopPage(pageID); err != nil {
-					cumulatedErrors = multierror.Append(cumulatedErrors, errors.Wrapf(err, "Failed to stop page \"%s\"", pageID))
-					// continue to remove it
-				}
-
-				// remove page
-				delete(m.startedPages, pageID)
-
-				return cumulatedErrors
-			}
-
-			widgetIDs[id.WidgetIndex(widgetIndex)] = widgetID
-		}
-
-		for widgetIndex, widget := range page.Widgets {
-			if !widget.IsInteractive() {
-				continue
-			}
-
-			widgetID := widgetIDs[id.WidgetIndex(widgetIndex)]
-
-			go func(widget *parser.Widget, widgetID id.WidgetID) {
-				for {
-					data, err := m.executor.Read(widgetID)
-					if err != nil {
-						if err == io.EOF {
-							return
-						}
-
-						log.Print(err) // there is no error channel to the clients, just log it
-						return
-					}
-
-					// lock page to allow safe access of attached clients
-					m.startedPagesLocks.lock(pageID)
-
-					var writeWaiting sync.WaitGroup
-					writeWaiting.Add(len(attachedClients))
-					for clientIndex := range attachedClients {
-						go func(clientIndex int, writeWaiting *sync.WaitGroup) {
-							defer writeWaiting.Done()
-
-							if err := attachedClients[clientIndex].Write(widgetID, data); err != nil {
-								log.Print(err) // there is no error channel to the clients, just log it
-							}
-						}(clientIndex, &writeWaiting)
-					}
-
-					m.startedPagesLocks.unlock(pageID)
-
-					writeWaiting.Wait()
-				}
-			}(&page.Widgets[widgetIndex], widgetID)
-		}
-	}
-
-	attachedClients = append(attachedClients, client)
-
-	go func() {
-		defer func() {
-			m.startedPagesLocks.lock(pageID)
-			defer m.startedPagesLocks.unlock(pageID)
-
-			// detach client
-			n := 0
-			for _, c := range attachedClients {
-				if c != client {
-					attachedClients[n] = c
-					n++
-				}
-			}
-			attachedClients = attachedClients[:n]
-
-			// if last client closed, close page
-			if len(attachedClients) == 0 {
-				// this lock will not introduce any deadlock, since we currently have the lock of this page (correct locking order)
-				m.startedPagesMutex.Lock()
-				defer m.startedPagesMutex.Unlock()
-
-				if err := m.executor.StopPage(pageID); err != nil {
-					log.Print(err) // there is no error channel to the clients, just log it
-					// continue to remove it
-				}
-
-				// remove page
-				delete(m.startedPages, pageID)
-			}
-		}()
-
-		for {
-			widgetID, data, err := client.Read()
-			if err != nil {
-				if err == io.EOF {
-					return
-				}
-
-				log.Print(err) // there is no error channel to the clients, just log it
-				return
-			}
-
-			if err := m.executor.Write(widgetID, data); err != nil {
-				log.Print(err) // there is no error channel to the clients, just log it
-				continue
-			}
-		}
-	}()
 
 	return nil
 }
