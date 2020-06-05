3c758f33cb3b03c7a2dddde12a4bce2f4c7519f1
Hendrik <dev.nipe.systems@gmail.com>
2020-06-05 16:08:45 +0200

    Style editor widget

--- frontend/src/page/widget/EditorWidget.js
+++ frontend/src/page/widget/EditorWidget.js
@@ -1,22 +1,33 @@
 import React from 'react';
 import AceEditor from 'react-ace';
+import seeNoEvilMonkeyEmoji from './see-no-evil-monkey.png';
 
 export default function EditorWidget(props) {
+  const showError = props.widget.error !== null || props.widget.contents === null;
+
   return (
     <div className="centered editor">
-      <div className="file">{props.widget.file}</div>
-      {props.widget.contents ?
-        <AceEditor
-          className="editor"
-          name="ace-editor"
-          mode=""
-          theme=""
-          value={props.widget.contents}
-          onChange={props.onChange}
-        />
-      :
-        <div className="error">Error: {props.widget.error}</div>
-      }
+      <div className={showError ? 'contents with-error' : 'contents with-contents'}>
+        {showError ?
+          <div className="error">
+            <img src={seeNoEvilMonkeyEmoji} alt="Oops" />
+            <div className="title">Cannot view <code>{props.widget.file}</code></div>
+            <div className="description">{props.widget.error !== null ? props.widget.error : 'There is no data'}</div>
+          </div>
+          :
+          <AceEditor
+            className="editor"
+            name="ace-editor"
+            mode=""
+            theme=""
+            width="100%"
+            height="100%"
+            value={props.widget.contents}
+            onChange={props.onChange}
+          />
+        }
+      </div>
+      <div className="caption">Edit text contents of <code>{props.widget.file}</code></div>
     </div>
   );
 }
