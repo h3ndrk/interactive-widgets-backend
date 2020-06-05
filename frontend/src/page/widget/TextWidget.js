899dbc70c2e9c3c49d60339b85fb3937daef9689
Hendrik <dev.nipe.systems@gmail.com>
2020-06-05 10:19:42 +0200

    Style text widget

--- frontend/src/page/widget/TextWidget.js
+++ frontend/src/page/widget/TextWidget.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import seeNoEvilMonkeyEmoji from './see-no-evil-monkey.png';
 
 export default function TextWidget(props) {
+  const showError = props.widget.error !== null || props.widget.contents === null;
+  
   return (
     <div className="centered text">
-      <div className="file">{props.widget.file}</div>
-      {props.widget.contents ?
-        <div className="contents">Contents: {props.widget.contents}</div>
-      :
-        <div className="error">Error: {props.widget.error}</div>
-      }
+      <div className={showError ? 'contents with-error' : 'contents with-contents'}>
+        {showError ?
+          <div className="error">
+            <img src={seeNoEvilMonkeyEmoji} alt="Oops" />
+            <div className="title">Cannot view <code>{props.widget.file}</code></div>
+            <div className="description">{props.widget.error !== null ? props.widget.error : 'There is no data'}</div>
+          </div>
+        :
+          props.widget.contents.split('\n').map((line, i) =>
+            <React.Fragment key={i}>
+              <div className="line-number">{i + 1}</div>
+              <div className="line">{line}</div>
+            </React.Fragment>
+          )
+        }
+      </div>
+      <div className="caption">Viewing text contents of <code>{props.widget.file}</code></div>
     </div>
   );
 }
