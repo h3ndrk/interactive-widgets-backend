a53b5b90a2f2dba20f2a8f168059c740ec00f41a
Hendrik <dev.nipe.systems@gmail.com>
2020-06-05 14:04:26 +0200

    Style button widget

--- frontend/src/page/widget/ButtonWidget.js
+++ frontend/src/page/widget/ButtonWidget.js
@@ -3,14 +3,16 @@ import React from 'react';
 export default function ButtonWidget(props) {
   return (
     <div className="centered button">
-      <div className="command">{props.widget.command}</div>
       <button className="button" onClick={() => {
         if (props.onClick) {
           props.onClick();
         }
       }}>{props.widget.label}</button>
+      <div className="command">{props.widget.command}</div>
       <div className="outputs">
-        {props.widget.outputs.map((output, i) => <div className={output.origin} key={i}>{output.data}</div>)}
+        {props.widget.outputs.map((output, i) =>
+          <div className={output.origin} key={i}>{output.data}</div>
+        )}
       </div>
     </div>
   );
