9d7cb90cbc40842f41eb4909ab93e14300b1732d
Hendrik <dev.nipe.systems@gmail.com>
2020-06-05 17:24:31 +0200

    Rewrite error outputting of monitor-write in JSON

--- cmd/monitor_write/main.go
+++ cmd/monitor_write/main.go
@@ -2,19 +2,60 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"os"
 	"os/signal"
+	"strconv"
 	"syscall"
 	"time"
 
 	"github.com/h3ndrk/containerized-playground/internal/fileio"
-	"github.com/pkg/errors"
 )
 
+// type jsonError struct {
+//     Type        string `json:"type"` // always "jsonError"
+//     ErrorReason string `json:"errorReason"`
+// }
+
+type argumentError struct {
+	Type          string   `json:"type"` // always "argumentError"
+	ExpectedCount int      `json:"expectedCount"`
+	GotCount      int      `json:"gotCount"`
+	GotArguments  []string `json:"gotArguments"`
+}
+
+type stdinReadError struct {
+	Type        string `json:"type"` // always "stdinReadError"
+	ErrorReason string `json:"errorReason"`
+}
+
+type removalError struct {
+	Type        string `json:"type"` // always "removalError"
+	Path        string `json:"path"`
+	ErrorReason string `json:"errorReason"`
+}
+
+type contents struct {
+	Type     string `json:"type"` // always "contents"
+	Contents string `json:"contents"`
+}
+
+type removal struct {
+	Type string `json:"type"` // always "removal"
+}
+
 func main() {
+	stdoutEncoder := json.NewEncoder(os.Stdout)
 	if len(os.Args) != 2 {
-		fmt.Fprintln(os.Stderr, "Expected 2 parameters, got", len(os.Args), "parameters:", os.Args)
+		if err := stdoutEncoder.Encode(&argumentError{
+			Type:          "argumentError",
+			ExpectedCount: 2,
+			GotCount:      len(os.Args),
+			GotArguments:  os.Args,
+		}); err != nil {
+			fmt.Printf("{\"type\":\"jsonError\",\"errorReason\":%s}\n", strconv.Quote(err.Error()))
+		}
 		return
 	}
 	pathToWatch := os.Args[1]
@@ -23,42 +64,66 @@ func main() {
 	signals := make(chan os.Signal, 1)
 	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
 	go func() {
-		fmt.Fprintln(os.Stderr, "Got ", <-signals)
+		<-signals
 		close(done)
 	}()
 
 	go func() {
+		stderrEncoder := json.NewEncoder(os.Stderr)
 		scanner := bufio.NewScanner(os.Stdin)
 
+	stdinLoop:
 		for scanner.Scan() {
-			err := fileio.WriteFileFromBase64(pathToWatch, scanner.Text())
-			if err != nil {
-				fmt.Fprintln(os.Stderr, err)
-				break
+			var typeMessage struct {
+				Type string `json:"type"`
+			}
+			if err := json.Unmarshal(scanner.Bytes(), &typeMessage); err != nil {
+				fmt.Fprintf(os.Stderr, "{\"type\":\"jsonError\",\"errorReason\":%s}\n", strconv.Quote(err.Error()))
+				continue
+			}
+
+			switch typeMessage.Type {
+			case "contents":
+				var contentsMessage contents
+				if err := json.Unmarshal(scanner.Bytes(), &contentsMessage); err != nil {
+					fmt.Fprintf(os.Stderr, "{\"type\":\"jsonError\",\"errorReason\":%s}\n", strconv.Quote(err.Error()))
+					continue stdinLoop
+				}
+
+				if err := fileio.WriteFileFromBase64(pathToWatch, scanner.Text()); err != nil {
+					fmt.Println(err.Error())
+					continue stdinLoop
+				}
+			case "removal":
+				if err := os.Remove(pathToWatch); err != nil {
+					if err := stderrEncoder.Encode(&removalError{
+						Type:        "removalError",
+						Path:        pathToWatch,
+						ErrorReason: err.Error(),
+					}); err != nil {
+						fmt.Fprintf(os.Stderr, "{\"type\":\"jsonError\",\"errorReason\":%s}\n", strconv.Quote(err.Error()))
+					}
+					continue stdinLoop
+				}
 			}
 		}
 		if err := scanner.Err(); err != nil {
-			fmt.Fprintln(os.Stderr, errors.Wrap(err, "Error while reading stdin"))
+			if err := stderrEncoder.Encode(&stdinReadError{
+				Type:        "stdinReadError",
+				ErrorReason: err.Error(),
+			}); err != nil {
+				fmt.Fprintf(os.Stderr, "{\"type\":\"jsonError\",\"errorReason\":%s}\n", strconv.Quote(err.Error()))
+			}
 		}
 	}()
 
 	lastEncoded := ""
 	didOutputAtLeastOnce := false
-loop:
+stdoutLoop:
 	for {
 		encoded, err := fileio.ReadFileToBase64(pathToWatch)
-		// encoded will be empty or containing the base64 string
-		if encoded != lastEncoded {
-			didOutputAtLeastOnce = true
-			fmt.Println(encoded)
-			lastEncoded = encoded
-		} else if !didOutputAtLeastOnce && encoded == "" {
-			didOutputAtLeastOnce = true
-			fmt.Println(encoded)
-		}
-
 		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
+			fmt.Println(err.Error())
 			timer := time.NewTimer(5 * time.Second)
 			select {
 			case <-timer.C:
@@ -66,13 +131,33 @@ loop:
 				if !timer.Stop() {
 					<-timer.C
 				}
-				break loop
+				break stdoutLoop
 			}
 			continue
 		}
 
+		// encoded will be empty or containing the base64 string
+		if encoded != lastEncoded {
+			didOutputAtLeastOnce = true
+			if err := stdoutEncoder.Encode(&contents{
+				Type:     "contents",
+				Contents: encoded,
+			}); err != nil {
+				fmt.Printf("{\"type\":\"jsonError\",\"errorReason\":%s}\n", strconv.Quote(err.Error()))
+			}
+			lastEncoded = encoded
+		} else if !didOutputAtLeastOnce && encoded == "" {
+			didOutputAtLeastOnce = true
+			if err := stdoutEncoder.Encode(&contents{
+				Type:     "contents",
+				Contents: encoded,
+			}); err != nil {
+				fmt.Printf("{\"type\":\"jsonError\",\"errorReason\":%s}\n", strconv.Quote(err.Error()))
+			}
+		}
+
 		if err := fileio.WaitForEvent(pathToWatch, done); err != nil {
-			fmt.Fprintln(os.Stderr, err)
+			fmt.Println(err.Error())
 			timer := time.NewTimer(5 * time.Second)
 			select {
 			case <-timer.C:
@@ -80,7 +165,7 @@ loop:
 				if !timer.Stop() {
 					<-timer.C
 				}
-				break loop
+				break stdoutLoop
 			}
 			continue
 		}
@@ -88,7 +173,7 @@ loop:
 		select {
 		case _, ok := <-done:
 			if !ok {
-				break loop
+				break stdoutLoop
 			}
 		default:
 		}
