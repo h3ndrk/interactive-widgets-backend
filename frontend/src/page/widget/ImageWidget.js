ac05d3a440d48a642caa52862608b0f5e2444dc1
Hendrik <dev.nipe.systems@gmail.com>
2020-06-05 10:47:32 +0200

    Style image widget

--- frontend/src/page/widget/ImageWidget.js
+++ frontend/src/page/widget/ImageWidget.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import seeNoEvilMonkeyEmoji from './see-no-evil-monkey.png';
 
 export default function ImageWidget(props) {
+  const showError = props.widget.error !== null || props.widget.contents === null;
+  console.log('showError:', showError);
+  const contentsStyle = showError ? {} : { backgroundImage: `url(data:${props.widget.mime};base64,${props.widget.contents})` };
+  console.log('contentsStyle:', contentsStyle);
+
   return (
     <div className="centered image">
-      <div className="file">{props.widget.file}</div>
-      {props.widget.contents ?
-        <img className="contents" src={`data:${props.widget.mime};base64,${props.widget.contents}`} alt={`Contents of ${props.widget.file}`} />
-      :
-        <div className="error">Error: {props.widget.error}</div>
-      }
+      <div className={showError ? 'contents with-error' : 'contents with-contents'} style={contentsStyle}>
+        {showError &&
+          <div className="error">
+            <img src={seeNoEvilMonkeyEmoji} alt="Oops" />
+            <div className="title">Cannot view <code>{props.widget.file}</code></div>
+            <div className="description">{props.widget.error !== null ? props.widget.error : 'There is no data'}</div>
+          </div>
+        }
+      </div>
+      <div className="caption">Viewing image <code>{props.widget.file}</code></div>
     </div>
   );
 }
