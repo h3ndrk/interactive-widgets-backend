de342c51f7e78a9d1661476ac080578256c2a70d
Hendrik <dev.nipe.systems@gmail.com>
2020-10-31 12:05:30 +0100

    Implement asynchronous triggering

--- inter_md/backend/executors/docker_trigger.py
+++ inter_md/backend/executors/docker_trigger.py
@@ -1,3 +1,6 @@
+import asyncio
+import base64
+import traceback
 import typing
 
 from .. import executors
@@ -5,13 +8,36 @@ from .. import executors
 
 class DockerTrigger(executors.DockerExecutor):
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.execute_task: typing.Optional[asyncio.Task] = None
+
+    async def _execute(self):
+        try:
+            await executors.docker_once(
+                self.context.docker,
+                self.name,
+                self.configuration['command'],
+                self.configuration['image'],
+                self.volume.name,
+                self.logger,
+                self.send_message,
+            )
+        except:
+            await self.send_message({
+                'stderr': base64.b64encode(traceback.format_exc().encode('utf-8')).decode('utf-8'),
+            })
+        finally:
+            self.execute_task = None
+
     async def handle_message(self, message: typing.Any):
-        await executors.docker_once(
-            self.context.docker,
-            self.name,
-            self.configuration['command'],
-            self.configuration['image'],
-            self.volume.name,
-            self.logger,
-            self.send_message,
-        )
+        self.execute_task = asyncio.create_task(self._execute())
+
+    async def tear_down(self):
+        if self.execute_task is not None:
+            self.execute_task.cancel()
+            try:
+                await self.execute_task
+            except asyncio.CancelledError:
+                pass
