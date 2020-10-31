4bc7fc82a5f03c65f4f4bd197152ad041e81ac65
Hendrik <dev.nipe.systems@gmail.com>
2020-10-31 14:30:17 +0100

    Refactor docker once executor

--- inter_md/backend/executors/__init__.py
+++ inter_md/backend/executors/__init__.py
@@ -1,6 +1,5 @@
-from .docker_once import docker_once
-
-from .docker_executor import DockerExecutor
-from .docker_prologue import DockerPrologue
+from .docker_always import DockerAlways
 from .docker_epilogue import DockerEpilogue
-from .docker_trigger import DockerTrigger
+from .docker_executor import DockerExecutor
+from .docker_once import docker_once, DockerOnce
+from .docker_prologue import DockerPrologue
--- inter_md/backend/executors/docker_once.py
+++ inter_md/backend/executors/docker_once.py
@@ -1,9 +1,13 @@
 import aiodocker
+import asyncio
 import base64
 import binascii
 import collections
 import logging
+import traceback
 import typing
+
+from .. import executors
 
 
 async def docker_once(docker: aiodocker.Docker, name: str, command: typing.List[str], image: str, volume_name: str, logger: logging.Logger, write_output: collections.abc.Coroutine):
@@ -44,3 +48,43 @@ async def docker_once(docker: aiodocker.Docker, name: str, command: typing.List[
     finally:
         logger.debug('Deleting container...')
         await container.delete(force=True)
+
+
+class DockerOnce(executors.DockerExecutor):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.execute_task: typing.Optional[asyncio.Task] = None
+
+    async def _execute(self):
+        try:
+            await self.send_message({
+                'triggered': True,
+            })
+            await executors.docker_once(
+                self.context.docker,
+                self.name,
+                self.configuration['command'],
+                self.configuration['image'],
+                self.volume.name,
+                self.logger,
+                self.send_message,
+            )
+        except:
+            await self.send_message({
+                'stderr': base64.b64encode(traceback.format_exc().encode('utf-8')).decode('utf-8'),
+            })
+        finally:
+            self.execute_task = None
+
+    async def handle_message(self, message: typing.Any):
+        self.execute_task = asyncio.create_task(self._execute())
+
+    async def tear_down(self):
+        if self.execute_task is not None:
+            self.execute_task.cancel()
+            try:
+                await self.execute_task
+            except asyncio.CancelledError:
+                pass
