cd9ed79b4ca8fdea61cd3d3215fee4728da421c6
Hendrik <dev.nipe.systems@gmail.com>
2020-11-01 06:19:47 +0100

    Reorder modules to prevent circular dependency

--- inter_md/backend/executors/__init__.py
+++ inter_md/backend/executors/__init__.py
@@ -1,5 +1,5 @@
+from .docker_executor import DockerExecutor
 from .docker_always import DockerAlways
 from .docker_epilogue import DockerEpilogue
-from .docker_executor import DockerExecutor
-from .docker_once import docker_once, DockerOnce
+from .docker_once import DockerOnce
 from .docker_prologue import DockerPrologue
