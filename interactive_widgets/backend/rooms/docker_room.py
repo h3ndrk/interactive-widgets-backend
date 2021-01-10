5ca78b343a628e1c73fb6399a035c55b6d80fcd0
Hendrik <git@h3ndrk.de>
2021-01-10 10:23:43 +0100

    Improve exception handling in docker room

--- interactive_widgets/backend/rooms/docker_room.py
+++ interactive_widgets/backend/rooms/docker_room.py
@@ -1,7 +1,9 @@
 import aiodocker
+import asyncio
 import binascii
 import collections
 import logging
+import traceback
 import typing
 
 import interactive_widgets.backend.contexts.docker_context
@@ -41,6 +43,8 @@ class DockerRoom(interactive_widgets.backend.rooms.room.Room):
         }
 
     async def instantiate(self):
+        # If an exception is raised within this function, this room will be torn down afterwards
+        # (no need to revert instantiation here).
         self.logger.debug('Waiting for tear down...')
         await self.state.wait_for_tear_down()
 
@@ -64,11 +68,29 @@ class DockerRoom(interactive_widgets.backend.rooms.room.Room):
     async def tear_down(self):
         self.logger.debug('Tearing down...')
         self.state.clear_instantiated()
-        for executor_name, executor in self.executors.items():
-            self.logger.debug(f'Tearing down executor {executor_name}...')
-            await executor.tear_down()
-        if self.volume is not None:
-            await self.volume.delete()
-            self.volume = None
-        self.logger.info('Torn down.')
-        self.state.set_torn_down()
+        try:
+            try:
+                self.logger.debug(f'Tearing down executors {", ".join(self.executors.keys())}...')
+                results = await asyncio.gather(
+                    *(
+                        executor.tear_down()
+                        for executor in self.executors.values()
+                    ),
+                    return_exceptions=True,
+                )
+                exceptions = [result for result in results if result is not None]
+                if len(exceptions) > 0:
+                    for exception in exceptions:
+                        traceback.print_exception(
+                            type(exception),
+                            exception,
+                            exception.__traceback__,
+                        )
+                    raise RuntimeError('Failed to tear down room\'s executors')
+            finally:
+                if self.volume is not None:
+                    await self.volume.delete()
+                    self.volume = None
+        finally:
+            self.logger.info('Torn down.')
+            self.state.set_torn_down()
