2e4f1c6346981a3c4e14ddcc467733e101269ddd
Hendrik <git@h3ndrk.de>
2021-01-11 11:06:42 +0100

    Simplify DockerAlways

--- interactive_widgets/backend/executors/docker_always.py
+++ interactive_widgets/backend/executors/docker_always.py
@@ -15,7 +15,7 @@ class DockerAlways(interactive_widgets.backend.executors.docker_executor.DockerE
         self.execute_task: typing.Optional[asyncio.Task] = None
         self.container: typing.Optional[aiodocker.docker.DockerContainer] = None
         self.stream: typing.Optional[aiodocker.stream.Stream] = None
-        self.should_terminate = False
+        self.stream_ready = asyncio.Event()
 
     async def instantiate(self, *args, **kwargs):
         await super().instantiate(*args, **kwargs)
@@ -23,7 +23,7 @@ class DockerAlways(interactive_widgets.backend.executors.docker_executor.DockerE
         self.execute_task = asyncio.create_task(self._execute())
 
     async def _execute(self):
-        while not self.should_terminate:
+        while True:
             try:
                 self.logger.debug('Creating container...')
                 self.container = await self.context.docker.containers.create(
@@ -54,26 +54,29 @@ class DockerAlways(interactive_widgets.backend.executors.docker_executor.DockerE
                     try:
                         self.logger.debug('Starting container...')
                         await self.container.start()
+                        self.stream_ready.set()
                         while True:
                             message = await stream.read_out()
                             if message is None:
                                 break
                             assert message.stream in [1, 2]
-
                             await self.send_message({
                                 'type': 'output',
                                 'stdout' if message.stream == 1 else 'stderr': base64.b64encode(message.data).decode('utf-8')
                             })
                     finally:
+                        self.stream_ready.clear()
                         self.stream = None
             finally:
+                self.logger.debug('Stopping container...')
+                await self.container.stop()
                 self.logger.debug('Deleting container...')
                 await self.container.delete(force=True)
                 self.container = None
 
     async def handle_message(self, message: dict):
+        await asyncio.wait_for(self.stream_ready.wait(), self.configuration.get('handle_message_timeout', 10))
         assert self.container is not None
-        assert self.stream is not None
         if 'stdin' in message:
             await self.stream.write_in(
                 base64.b64decode(message['stdin'].encode('utf-8')),
@@ -88,21 +91,10 @@ class DockerAlways(interactive_widgets.backend.executors.docker_executor.DockerE
             raise NotImplementedError
 
     async def tear_down(self):
-        self.should_terminate = True
-        try:
-            if self.container is not None:
-                self.logger.debug('Stopping container...')
-                await self.container.stop()
-        finally:
-            if self.execute_task is not None:
-                try:
-                    self.logger.debug('Waiting for execute task...')
-                    await asyncio.wait_for(self.execute_task, self.configuration.get('tear_down_timeout', 10))
-                except asyncio.TimeoutError:
-                    self.logger.debug(
-                        'Timeout while waiting for execute task, cancelling...')
-                    self.execute_task.cancel()
-                    try:
-                        await self.execute_task
-                    except asyncio.CancelledError:
-                        pass
+        if self.execute_task is not None:
+            self.execute_task.cancel()
+            self.logger.debug('Waiting for execute task...')
+            try:
+                await self.execute_task
+            except asyncio.CancelledError:
+                pass
